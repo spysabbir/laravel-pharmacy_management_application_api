04c9071f40532b72b382dadccb5bc9be1962d661
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-16 12:51:19 +0600

    Setup Panel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\MedicineController;
+use App\Http\Controllers\Api\PowerController;
 use App\Http\Controllers\Api\SupplierController;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\UnitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +26,11 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::resource('suppliers', SupplierController::class);
+    Route::resource('customer', CustomerController::class);
+    Route::resource('type', TypeController::class);
+    Route::resource('power', PowerController::class);
+    Route::resource('unit', UnitController::class);
+    Route::resource('medicine', MedicineController::class);
 
     Route::get('logout', [AuthController::class, 'logout']);
-
 });
